9a91a9d3fba0ccc9ecba7c3e7fcdd4faf0342675
Aeerien <aeerien@icloud.com>
2025-02-26 14:20:44 +0300

    Initial Commit

---
+++ RickandmortyDemo/RickandmortyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandmortyDemoApp.swift
+//  RickandmortyDemo
+//
+//  Created by Irina Arkhireeva on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RickandmortyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
